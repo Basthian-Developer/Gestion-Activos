98ea6ab8be9b9d9517009cd11e9fd6d201e11d0f
Basthian-Developer <basthianfmillan@gmail.com>
2026-08-28 04:39:53 -0400

    Arreglo 8

--- api/index.py
+++ api/index.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 
+from features.test import health
+
 app = FastAPI()
 
 @app.get("/api")
 def root():
     return {"message": "API funcionando"}
+
+app.include_router(
+    health.router,
+    prefix="/api/health",
+    tags=["Health"]
+)
