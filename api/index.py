fa600736b1696d20e04a6015cd907f1e3ae0213b
Basthian-Developer <basthianfmillan@gmail.com>
2026-08-28 04:31:44 -0400

    Intento 6 Vercel.json

--- api/index.py
+++ api/index.py
@@ -1,15 +1,7 @@
 from fastapi import FastAPI
 
-from features.test import health
-
 app = FastAPI()
 
 @app.get("/api")
 def root():
     return {"message": "API funcionando"}
-
-app.include_router(
-    health.router,
-    prefix="/api/health",
-    tags=["Health"]
-)
