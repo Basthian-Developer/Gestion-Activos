a8e6b0d4793c00bc489bc241bff5d81ff13612a2
Basthian-Developer <basthianfmillan@gmail.com>
2026-08-28 06:55:28 -0400

    Intento 15

--- api/features/test/health.py
+++ api/features/test/health.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/api/health",
+    tags=["Health"]
+)
 
 @router.get("/")
 def health():
--- api/index.py
+++ api/index.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
-import sys
-from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parent))
-
-from features.test import health
+from features.test.health import router as healthRouter
 
 app = FastAPI()
 
@@ -12,12 +8,4 @@ app = FastAPI()
 def root():
     return {"message": "API funcionando"}
 
-@app.get("/api/prueba")
-def prueba():
-    return {"message": "prueba funcionando"}
-
-app.include_router(
-    health.router,
-    prefix="/api/health",
-    tags=["Health"]
-)
+app.include_router(healthRouter)
