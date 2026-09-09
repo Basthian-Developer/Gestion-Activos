410de4ed134d6c02bb599c14e84f2d8c487a4b95
Basthian-Developer <basthianfmillan@gmail.com>
2026-09-09 20:12:47 -0300

    Arreglo del vite.config.ts, package.json y deploy.yml

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
   server: {
     host: '0.0.0.0', port: 3000, proxy: {
@@ -14,5 +14,5 @@ export default defineConfig({
       }
     }
   },
-  base: '/Gestion-Activos/'
-})
+  base: mode === 'github-pages' ? '/Gestion-Activos/' : '/'
+}))
